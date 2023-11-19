920494782f915e1422ad9a6ed846620d7aef2260
suraj <surajrajapandian@gmail.com>
2023-11-19 14:50:29 +0530

    19-11

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Voters
 
 # Register your models here.
+
+admin.site.register(Voters)
--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Voters
 # Create your views here.
 
 def index(request):
@@ -9,10 +9,18 @@ def index(request):
 def home(request):
     if request.method == 'POST':
         age = request.POST.get('age')
+        name = request.POST.get('voter_name')
+        is_voted = False
+        
         data = {'age':0}
 
         if int(age) >= 18:
             data['age'] = 1
+            is_voted = True
+        
+        obj = Voters.objects.create(name = name, age=age, is_voted = is_voted)
+        obj.save()
+
 
 
         # data = {'cities':
